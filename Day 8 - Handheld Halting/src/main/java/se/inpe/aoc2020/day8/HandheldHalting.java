53264871e695360d31c6cf42aeb97ee6a15f43ce
inpe <plingpersson@hotmail.com>
2021-02-23 15:37:46 +0100

    AOC Day8 HandheldHalting Part 2

--- Day 8 - Handheld Halting/src/main/java/se/inpe/aoc2020/day8/HandheldHalting.java
+++ Day 8 - Handheld Halting/src/main/java/se/inpe/aoc2020/day8/HandheldHalting.java
@@ -3,6 +3,7 @@ package se.inpe.aoc2020.day8;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
@@ -10,6 +11,7 @@ import java.util.stream.Stream;
 
 public class HandheldHalting {
     final Map<Integer, ProgramCode> programLines = new HashMap<>();
+    final ArrayList<Integer> lineNumbersWithNopOrJmp = new ArrayList<>();
     int accumulator = 0;
 
     public static void main(String[] args) {
@@ -20,34 +22,84 @@ public class HandheldHalting {
         try (Stream<String> stream = Files.lines(Paths.get("src/main/resources/day8-HandheldHalting.txt"))) {
             AtomicReference<Integer> i = new AtomicReference<>(1);
             stream.forEach(line -> addToProgramLines(i.getAndSet(i.get() + 1), line));
-            runBootCode(1);
+            programLines.forEach((number, code) -> addToLineNumbersWithNopOrJmp(number, code.getOperation()) );
+
+            //Part 1
+            runBootCode(1, true);
+
+            //Part 2
+            boolean exitedNormally = false;
+            while (!exitedNormally && !lineNumbersWithNopOrJmp.isEmpty()) {
+                accumulator = 0;
+                Integer lineWithNopOrJmp = lineNumbersWithNopOrJmp.remove(0);
+                programLines.forEach((number, code) -> code.resetTimesVisited());
+                switchNopAndJmp(lineWithNopOrJmp);
+                exitedNormally = runBootCode(1, false);
+                switchNopAndJmp(lineWithNopOrJmp);
+            }
+            System.out.println("SUCCESS!");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void addToProgramLines(Integer number, String fileLine) {
+    private void addToProgramLines(Integer lineNumber, String fileLine) {
         String[] operationAndNumber = fileLine.split(" ");
-        programLines.put(number, new ProgramCode(operationAndNumber[0],Integer.parseInt(operationAndNumber[1])));
+        programLines.put(lineNumber, new ProgramCode(operationAndNumber[0], Integer.parseInt(operationAndNumber[1])));
     }
 
-    private void runBootCode(int lineNumber) {
+    private void addToLineNumbersWithNopOrJmp(Integer number, String operation) {
+        if (operation.equalsIgnoreCase(ProgramCode.JMP) || operation.equalsIgnoreCase(ProgramCode.NOP)) {
+            lineNumbersWithNopOrJmp.add(number);
+        }
+    }
+
+    private void switchNopAndJmp(Integer lineNumber) {
         ProgramCode programCode = programLines.get(lineNumber);
 
-        if (programCode.getTimesVisited() == 1) {
-            System.out.println("Second run on this line: " +lineNumber + " Accumulator is: " +accumulator);
-            return;
+        if (programCode.getOperation().equalsIgnoreCase(ProgramCode.NOP)) {
+            programCode.setOperation(ProgramCode.JMP);
+        } else if (programCode.getOperation().equalsIgnoreCase(ProgramCode.JMP)){
+            programCode.setOperation(ProgramCode.NOP);
         }
-        programCode.increaseTimesVisited();
+    }
 
-        switch (programCode.getOperation()) {
-            case "nop" -> runBootCode(lineNumber+1);
-            case "acc" -> {
-                accumulator += programCode.getNumber();
-                runBootCode(lineNumber+1);
+    private boolean runBootCode(int lineNumber, boolean checkHaltingLine) {
+        //Part 2
+        if (lineNumber > programLines.size() + 1) {
+            System.out.println("Program did NOT exit normally, lineNumber is: " + lineNumber);
+            return false;
+        }
+        else if (lineNumber == programLines.size() + 1) {
+            System.out.println("Program exited normally, accumulator is: " + accumulator);
+            return true;
+        }
+        else {
+            ProgramCode programCode = programLines.get(lineNumber);
+
+            if (programCode.getTimesVisited() > 1) {
+                if (checkHaltingLine) {
+                    System.out.println("Second round on this line: "+lineNumber+" program is halting");
+                }
+                return false;
             }
-            case "jmp" -> runBootCode(lineNumber + programCode.getNumber());
-            default -> throw new IllegalStateException("Unexpected value: " + programCode.getOperation());
+            else {
+                programCode.increaseTimesVisited();
+                switch (programCode.getOperation()) {
+                    case "nop" -> {
+                        return runBootCode(lineNumber + 1, checkHaltingLine);
+                    }
+                    case "acc" -> {
+                        accumulator += programCode.getNumber();
+                        return runBootCode(lineNumber + 1, checkHaltingLine);
+                    }
+                    case "jmp" -> {
+                        return runBootCode(lineNumber + programCode.getNumber(), checkHaltingLine);
+                    }
+                    default -> throw new IllegalStateException("Unexpected value: " + programCode.getOperation());
+                }
+            }
         }
     }
 }
--- Day 8 - Handheld Halting/src/main/java/se/inpe/aoc2020/day8/ProgramCode.java
+++ Day 8 - Handheld Halting/src/main/java/se/inpe/aoc2020/day8/ProgramCode.java
@@ -1,9 +1,13 @@
 package se.inpe.aoc2020.day8;
 
 public class ProgramCode {
-    private final String operation;
-    private final int number;
+    private String operation;
+    private int number;
     private int timesVisited;
+
+    public static final String NOP = "nop";
+    public static final String ACC = "acc";
+    public static final String JMP = "jmp";
 
     public ProgramCode(String operation, int number) {
         this.operation = operation;
@@ -15,6 +19,10 @@ public class ProgramCode {
         return operation;
     }
 
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
     public int getNumber() {
         return number;
     }
@@ -24,5 +32,8 @@ public class ProgramCode {
     }
     public void increaseTimesVisited() {
         timesVisited += 1;
+    }
+    public void resetTimesVisited() {
+        timesVisited = 0;
     }
 }
